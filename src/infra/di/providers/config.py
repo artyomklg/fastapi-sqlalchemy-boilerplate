7467cb19f0eb27f7ebd2ff5245c4ab914100ac3d
artyomklg <artem0801200269@gmail.com>
2024-10-26 01:26:06 +0300

    fix: make get_main_config singleton fabric

--- src/infra/di/providers/config.py
+++ src/infra/di/providers/config.py
@@ -1,9 +1,9 @@
 from dishka import Provider, Scope, provide
 
-from src.infra.main_config import MainConfig
+from src.infra.main_config import MainConfig, get_main_config
 
 
 class ConfigProvider(Provider):
     scope = Scope.APP
 
-    main_config = provide(MainConfig)
+    main_config = provide(get_main_config, provides=MainConfig)
--- src/infra/main_config.py
+++ src/infra/main_config.py
@@ -1,5 +1,11 @@
 from dataclasses import dataclass, field
+from functools import lru_cache
 
 
 @dataclass
 class MainConfig: ...
+
+
+@lru_cache(1)
+def get_main_config():
+    return MainConfig()
