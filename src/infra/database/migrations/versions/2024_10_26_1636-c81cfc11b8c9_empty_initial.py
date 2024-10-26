3dda47f826b2f0757e13d47e2d4e1e2200bc06cc
artyomklg <artem0801200269@gmail.com>
2024-10-26 16:37:24 +0300

    feat: add empty initial migration

---
+++ src/infra/database/migrations/versions/2024_10_26_1636-c81cfc11b8c9_empty_initial.py
@@ -0,0 +1,23 @@
+"""empty initial
+
+Revision ID: c81cfc11b8c9
+Revises:
+Create Date: 2024-10-26 16:36:05.421034
+
+"""
+
+from typing import Sequence, Union
+
+# revision identifiers, used by Alembic.
+revision: str = "c81cfc11b8c9"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
